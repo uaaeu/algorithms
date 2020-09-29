61ea1a1fb5299a3c8b9830942eb8111dd76154c5
uaaeu <yurtmusa@gmail.com>
2020-09-29 09:06:48 +0300

    implement-insertion-sort

---
+++ insertion-sort.js
@@ -0,0 +1,13 @@
+//Implement Insertion Sort
+function insertionSort(array) {
+  for(let i = 1; i < array.length; i++) {
+    let curr = array[i];
+    for(var j = i - 1; j >= 0 && array[j] > curr; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = curr
+  }
+  return array;
+}
+
+console.log(insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
